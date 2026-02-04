290be3017a51620a6a34a81d68f8808525faa1ad
juzi <juzi3336@gmail.com>
2026-02-04 08:05:53 +0800

    测试

--- plugins.v2/p123disk/__init__.py
+++ plugins.v2/p123disk/__init__.py
@@ -84,7 +84,7 @@ class P123Disk(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/juzi8633/MoviePilot-Plugins/main/icons/P123Disk.png"
     # 插件版本
-    plugin_version = "1.8.2"
+    plugin_version = "1.8.5"
     # 插件作者
     plugin_author = "juzi8633"
     # 作者主页
